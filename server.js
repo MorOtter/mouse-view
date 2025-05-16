eb6de299d609cb150a3b15f1c0fbc393c215798e
MorOtter <158565667+MorOtter@users.noreply.github.com>
2025-05-16 13:00:33 +0100

    reintroduction of functions

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const path = require('path'); // You might need this later
 const dbServices = require("./services/dbServices.js");
 
 const app = express();
 
-// This is the crucial part we are testing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log("Checking dbServices..."); // Add this log
-    console.log("Type of dbServices:", typeof dbServices); // And this log
+    console.log("Checking dbServices...");
+    console.log("Type of dbServices:", typeof dbServices);
     req.dbServices = dbServices;
     next();
 });
 
 app.get('/', (req, res) => {
-    res.send('This is a test!'); // Simple response
+    res.send('This is a test!');
 });
 
 const PORT = process.env.PORT || 3000;
