82fe9d307e16dd8f6ca14a2f140570048a535eeb
MorOtter <158565667+MorOtter@users.noreply.github.com>
2025-05-16 13:10:43 +0100

    reworking server to help heroku

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session'); // Add this back
+const session = require('express-session');
+const flash = require('express-flash');      // Add this back
+const methodOverride = require('method-override'); // And this
+
 const dbServices = require("./services/dbServices.js");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
@@ -15,6 +17,8 @@ app.use(
         saveUninitialized: false
     })
 );
+app.use(flash());
+app.use(methodOverride('_method'));
 
 app.use((req, res, next) => {
     console.log("Checking dbServices...");
