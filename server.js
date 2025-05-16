c5a3556bb0e193f88fd3c384b4eb0ca8d7014d8d
MorOtter <158565667+MorOtter@users.noreply.github.com>
2025-05-16 13:13:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-const flash = require('express-flash');      // Add this back
-const methodOverride = require('method-override'); // And this
-
+const flash = require('express-flash');
+const methodOverride = require('method-override');
+const participantDetailsRoutes = require("./routes/participantDetailsRoutes.js");
+const informationRoutes = require("./routes/informationRoutes.js");
+const scalesRoutes = require("./routes/scalesRoutes.js");
+const trialRoutes = require("./routes/trialRoutes.js");
 const dbServices = require("./services/dbServices.js");
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
@@ -17,6 +23,8 @@ app.use(
         saveUninitialized: false
     })
 );
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(flash());
 app.use(methodOverride('_method'));
 
@@ -27,9 +35,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => {
-    res.send('This is a test!');
-});
+app.use('/participant', participantDetailsRoutes);
+app.use('/information', informationRoutes);
+app.use('/scales', scalesRoutes);
+app.use('/trial', trialRoutes);
 
 const PORT = process.env.PORT || 3000;
 
