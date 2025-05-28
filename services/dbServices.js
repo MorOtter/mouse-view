9929309b8aef93825d0fad1bf7ab23ae8b03f4b4
MorOtter <158565667+MorOtter@users.noreply.github.com>
2025-05-28 14:49:23 +0100

    Update dbServices.js

--- services/dbServices.js
+++ services/dbServices.js
@@ -5,9 +5,9 @@ const { connect_timeout, idleTimeoutMillis } = require('pg/lib/defaults');
 
 // Set up connection to DB and CRUD ops
 
-// Supabase connection with transaction pooler
+// Supabase connection with transaction pooler (new password)
 const pool = new Pool({
-    connectionString: "postgresql://postgres.qxsjjkughdhjwgptbcih:Nig3l%40supbl22ue@aws-0-eu-west-2.pooler.supabase.com:6543/postgres",
+    connectionString: "postgresql://postgres.qxsjjkughdhjwgptbcih:bUxfq6cVNjekjAN9@aws-0-eu-west-2.pooler.supabase.com:6543/postgres",
     ssl: {
         rejectUnauthorized: false
     }
